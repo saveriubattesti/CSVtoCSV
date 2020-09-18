a67b4ec3bbaaa93009148bf24309ff94eefe7ef5
theoc2a <theocussac2a@gmail.com>
2020-09-18 14:51:20 +0200

    correction de bug avec delimiter

--- IHM/Form1.cs
+++ IHM/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Services;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DataContracts;
 
 namespace IHM
 {
@@ -15,6 +17,11 @@ namespace IHM
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Services.Source.
         }
     }
 }
