6c916f37afd301f5cb99f646a47fdca01bfca9cf
JEROME MARCHIONI <20180082@webmail.universita.corsica>
2020-09-18 10:22:27 +0200

    creating dev

--- IHM/Form1.Designer.cs
+++ IHM/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.listView_srcCSV = new System.Windows.Forms.ListView();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listView_srcCSV
@@ -50,11 +51,21 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "CSV Source";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(704, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CSV Destination";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listView_srcCSV);
             this.Name = "Form1";
@@ -68,6 +79,7 @@
 
         private System.Windows.Forms.ListView listView_srcCSV;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
