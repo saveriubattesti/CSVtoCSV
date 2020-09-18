ac7cd3fc2ab3d5f532d29c05796dc038283a7420
BLANC SEBASTIEN <20140277@webmail.universita.corsica>
2020-09-18 10:37:07 +0200

    test seb destination

--- Services/Destination.cs
+++ Services/Destination.cs
@@ -1,21 +1,41 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Services
+namespace TestProjet2
 {
-    public class Destination
+    class Destination
     {
-        public void addEntry()
+        public static void Submit(String PathTofile, String fileName, List<Block> blocks)
         {
-
+            using (var writer = new StreamWriter(PathTofile+fileName+".csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(blocks);
+            }
         }
 
-        public void Submit()
+        public static void Submit(String Path, List<Block> blocks)
         {
+            using (var writer = new StreamWriter(Path + ".csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(blocks);
+            }
+        }
 
+        public static void AddEntry(List<Block> blocks, String entryName)
+        {
+            int newBlockId = blocks.Count + 1;
+
+            blocks.Add(new Block() {Id = newBlockId, Entry = entryName });
         }
     }
 }
