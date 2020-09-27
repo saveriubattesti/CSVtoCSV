644baed5e64b978a9702bbe599e9da60768aec0f
Marc-Antoine-Lune <luneschima@gmail.com>
2020-09-27 20:05:09 +0200

    Ajout de deux fonctions makeColumns et mergeBis

--- Services/Merge.cs
+++ Services/Merge.cs
@@ -34,8 +34,34 @@ namespace Services
             outputBlocks.Add(
                  new Block() { Id = finalId, Entry = mergedString }
             );
-
+            
             return outputBlocks;
+        }
+
+        public static List<Block> MergeBis(List<List<Block>> colums, Int32[] idColumsShouldBeMerged, char separator)
+        {
+            List<Block> columnsMerged = new List<Block>();
+            List<List<Block>> allColums = new List<List<Block>>();
+            foreach (Int32 element in idColumsShouldBeMerged)
+            {
+                allColums.Add(colums[element]);
+            }
+            var nbColumns = idColumsShouldBeMerged.Length;
+            var idColumn = idColumsShouldBeMerged[0];
+            var n = 0;
+            
+            foreach(Block row in allColums[0])
+            {
+                for(int i = 1; i<nbColumns; i++)
+                {
+                 columnsMerged.Add(new Block { Id = n, Entry = row.Entry + separator + allColums[i][n].Entry });
+
+                }
+                n++;
+            }
+
+
+            return columnsMerged;
         }
     }
 }
--- Services/Source.cs
+++ Services/Source.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace Services
 {
@@ -29,10 +30,60 @@ namespace Services
                     lBlock.Add(new Block { Id = counterId, Entry = title });
                     counterId++;
                 }
+                MakeColunms(link);
                 return lBlock;
             }
-                    
 
+
+        }
+
+        public List<List<DataContracts.Block>> MakeColunms(String link)
+        {
+            using (var reader = new StreamReader(link))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Configuration.Delimiter = ";";
+                csv.Read();
+                csv.ReadHeader();
+                var nbCollumns = csv.Context.HeaderRecord.Length;
+                String value;
+                //var collumnId = 0;
+                var nb = 1;
+                List<List<Block>> collumns = new List<List<Block>>();
+                List<Block> listElement = new List<Block>();
+
+                while (csv.Read())
+                {
+                    for (int i = 0; csv.TryGetField<string>(i, out value); i++)
+                    {
+                            listElement.Add(new Block { Id = nb, Entry = value });
+                            nb++;
+                    }
+                }
+                var j = 1;
+
+                while (nbCollumns != 0)
+                {
+                    
+                    List<Block> collumn = new List<Block>();
+                    foreach (Block el in listElement)
+                    {
+                        if(el.Id%4 == j)
+                        {
+                            collumn.Add(el);
+                        }
+                        
+                    }
+                    j++;
+                    collumns.Add(collumn);
+                    nbCollumns--;
+                }
+
+
+                Merge.MergeBis(collumns, new int[] { 0, 1 }, '-');
+                return collumns;
+
+            }
         }
     }
 }
